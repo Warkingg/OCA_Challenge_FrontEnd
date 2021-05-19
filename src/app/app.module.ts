805f0e07b890778358dc4748dca6a9c44bcdf038
Tam250399 <tamthanhsk25@gmail.com>
2021-05-19 14:19:08 +0700

    last version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,6 +13,7 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegistrationComponent } from './registration/registration.component';
 import { ChangePasswordComponent } from './change-password/change-password.component';
+import {NgModule} from '@angular/core';
 
 @NgModule({
   declarations: [
