9a16a8dc8a7d7d06260af7037faf1f99bca2a473
Tam250399 <tamthanhsk25@gmail.com>
2021-05-19 09:13:50 +0700

    add-template-registration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,24 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {QuizListComponent} from './quiz/quiz-list/quiz-list.component';
 import {QuizCreateComponent} from './quiz/quiz-create/quiz-create.component';
+import {HomeComponent} from './home/home.component';
+import {LoginComponent} from './login/login.component';
+import {RegistrationComponent} from './registration/registration.component';
 
 
 const routes: Routes = [
+  {
+    path:'quiz',
+    component:HomeComponent
+  },
+  {
+    path:'login',
+    component:LoginComponent
+  },
+  {
+    path:'logout',
+    component:RegistrationComponent
+  },
   {
     path: 'quizzes', component: QuizListComponent
   },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import {FormsModule} from '@angular/forms';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { LoginComponent } from './login/login.component';
     NavBarComponent,
     SidebarComponent,
     HomeComponent,
-    LoginComponent
+    LoginComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
